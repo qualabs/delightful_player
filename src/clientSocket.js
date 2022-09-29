46321f5a7b763728b12da6eb3598dfa0b7243116
Paola Pintos <paolap@qualabs.com>
2022-09-29 15:32:01 -0300

    unifying plugin with subtitles

---
+++ src/clientSocket.js
@@ -0,0 +1,6 @@
+import * as config from "../config.js";
+
+const IP = config.SERVER_IP;
+const PORT = config.SERVER_PORT;
+const url = "ws://" + IP + ":" + PORT + "/";
+export const ws = new WebSocket(url);
--- src/plugin.js
+++ src/plugin.js
@@ -1,9 +1,10 @@
-import videojs from 'video.js';
-import {version as VERSION} from '../package.json';
-import {getColors, paintColors} from './Utils.js';
-import {getModo} from './getModo.js'
+import videojs from "video.js";
+import { version as VERSION } from "../package.json";
+import { getColors, paintColors } from "./Utils.js";
+import { getModo } from "./getModo.js";
+import { ws } from "./clientSocket.js";
 
-const Plugin = videojs.getPlugin('plugin');
+const Plugin = videojs.getPlugin("plugin");
 
 // Default options for the plugin.
 const defaults = {};
@@ -14,7 +15,6 @@ const defaults = {};
  * See: https://blog.videojs.com/feature-spotlight-advanced-plugins/
  */
 class DelightfulPlayer extends Plugin {
-
   /**
    * Create a DelightfulPlayer plugin instance.
    *
@@ -35,10 +35,10 @@ class DelightfulPlayer extends Plugin {
     this.options = videojs.mergeOptions(defaults, options);
 
     this.player.ready(() => {
-      this.player.addClass('vjs-delightful-player2');
+      this.player.addClass("vjs-delightful-player2");
     });
 
-    this.player.on('loadedmetadata', () => {
+    this.player.on("loadedmetadata", () => {
       let canvas = document.querySelector(".canvas");
       let video = this.player.tech_.el_;
       canvas.width = video.videoWidth;
@@ -47,23 +47,62 @@ class DelightfulPlayer extends Plugin {
       // videojs.log('canvas.height ' + canvas.height);
     });
 
-    this.player.on('playing', function() {
-      videojs.log('playback began!');
+    this.player.on("playing", function () {
+      videojs.log("playback began!");
     });
 
-    this.player.on('play', this.loop.bind(this, this.player));
+    this.player.on("canplay", this.textColor.bind(this, this.player));
     // player.controlBar.addChild('QualitySelector', options);
+  }
+
+  textColor(player) {
+    if (player.textTracks_.tracks_.length > 0) {
+      for (let i = 0; i < player.textTracks_.tracks_.length; i++) {
+        let track = player.textTracks_.tracks_[i];
+        if (track.src) {
+          this.sendColorFromTrack(player);
+        } else {
+          player.on("play", this.loop.bind(this, this.player));
+        }
+      }
+    }
+  }
+
+  sendColorFromTrack(player) {
+    // Get all text tracks for the current player.
+    let tracks = player.textTracks_.tracks_;
+
+    for (let i = 0; i < tracks.length; i++) {
+      let track = tracks[i];
+
+      track.oncuechange = () => {
+        if (track.activeCues.length > 0) {
+          // Parse the cue as JSON
+          let color = JSON.parse(track.activeCues[0].text);
+          console.log(color);
+          try {
+            const msg = JSON.stringify(color);
+            if (ws.readyState == 1) {
+              ws.send(msg);
+            }
+          } catch (error) {
+            console.log("Error when send package. Error: " + error);
+          }
+        }
+      };
+    }
   }
 
   loop(player) {
     let canvas = document.querySelector(".canvas");
-    let ctx = canvas.getContext('2d');
+    let ctx = canvas.getContext("2d");
 
     if (!player.paused() && !player.ended()) {
       ctx.drawImage(player.tech_.el_, 0, 0);
-      let modo   = getModo(document);
+      let modo = getModo(document);
 
       let jsonColor = getColors(modo);
+      console.log(jsonColor);
       paintColors(jsonColor);
       setTimeout(this.loop.bind(this, player), 1000 / 30); // drawing at 30fps
     }
@@ -77,6 +116,6 @@ DelightfulPlayer.defaultState = {};
 DelightfulPlayer.VERSION = VERSION;
 
 // Register the plugin with video.js.
-videojs.registerPlugin('delightfulPlayer', DelightfulPlayer);
+videojs.registerPlugin("delightfulPlayer", DelightfulPlayer);
 
 export default DelightfulPlayer;
